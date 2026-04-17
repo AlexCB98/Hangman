f5b79e26d31cd1a13357c90534df75b4bbe12ca2
Alexandru <lexyus22@gmail.com>
2026-04-17 10:15:53 +0200

    Implemented working Hangman logic.

--- main.py
+++ main.py
@@ -14,30 +14,39 @@ words = [
 ]
 
 word = random.choice(words)
+print(word)
 
+word_len = len(word)
 placeholder = ''
 for letter in word:
     placeholder += '_'
+print(f'Your word to guess have {placeholder}({word_len}) letters.')
 
 guessed = []
-game_over = True
+game_over = False
 lives = 5
 
 while not game_over:
-    guess = input('Choose a letter: ')
+    guess = input('Choose a letter to guess: ')
 
     display = ''
 
-    if guess in guessed:
-        display += guess
-    else:
+    if guess not in guessed:
         guessed.append(guess)
+    else:
+        print(f'You already guessed: "{guess}".')
 
-    for letter in guessed:
+    for letter in word:
         if letter in guessed:
             display += letter
         else:
             display += '_'
+
+    print(display)
+
+    if guess not in word:
+        lives -= 1
+        print('You lost a life.')
 
     if '_' not in display:
         game_over = True
